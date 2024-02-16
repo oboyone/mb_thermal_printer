ae4fdc9323b26b3ad00adc342d58e0fd5468248a
oboyone <alexanderjn@live.se>
2024-02-16 23:36:30 +0100

    Update convert_images_to_monochrome.sh

--- convert_images_to_monochrome.sh
+++ convert_images_to_monochrome.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-#folder containing card images
-cd "path-to-folder-containing-default-jpg-files" 
+# Folder containing cmc folders which contain card images
+IMAGE_ROOT="path-to-folder-containing-cmc-image-folders"
 
-# Create a new folder for the converted files
-mkdir -p converted_files
-
-# Iterate through each JPG file and convert it to a rescaled monochrome bitmap
-for file in *.jpg; do
-
-    echo "Resizing and converting to grayscale for: $file"
-
-    # Define the output filename by replacing the extension with bmp
-    output_file="converted_files/$(basename -- "$file" .jpg).bmp"
-    
-    # Use ImageMagick's convert command to perform the conversion
-    convert "$file" -resize 384x -colorspace Gray -monochrome "$output_file"
+# Iterate through all subdirectories
+for dir in "${IMAGE_ROOT}"/*; do
+    # Check if directory is empty
+    if [ -d "$dir" ] && [ "$(ls -A "$dir")" ]; then
+        # Create a new folder for the converted files
+        mkdir -p "${dir}/converted_files"
+        
+        # Iterate through each JPG file and convert it to a rescaled monochrome bitmap
+        for jpg_file in "${dir}"/*.jpg; do
+            if [ -f "$jpg_file" ]; then
+                echo "Resizing and converting to grayscale for: $jpg_file"
+                
+                # Define the output filename by replacing the extension with bmp
+                output_file="${dir}/converted_files/$(basename -- "$jpg_file" .jpg).bmp"
+                
+                # Use ImageMagick's convert command to perform the conversion
+                convert "$jpg_file" -resize 384x -colorspace Gray -monochrome "$output_file"
+                
+                # Check if conversion was successful
+                if [ $? -eq 0 ]; then
+                    echo "Conversion successful: $jpg_file"
+                else
+                    echo "Error converting: $jpg_file"
+                fi
+            fi
+        done
+    else
+        echo "No JPG files found in directory: $dir"
+    fi
 done
